1df947e97ab9d2057bb5ae6c3e94bf5c3d2f87c5
Clint Bloomquist <clintbloomquist@gmail.com>
2022-01-30 13:07:22 -0700

    Updated layout, fixed CreateLabel generator

--- main.py
+++ main.py
@@ -13,55 +13,38 @@ window.geometry("400x200")
 window.resizable(False, False)
 
 
-class LabelUpdate:
-    def __init__(self):
-        self.common = Label(window, text='hello')
+class CreateLabel:
+    amount = 0
 
-    def common_command(self):
-        self.common.grid(row=0, column=3)
+    def __init__(self, todo):
+        if todo == '':
+            print('No task assigned')
+        else: 
+            Label(window, text=todo).grid(row=CreateLabel.amount, column=3)
+            CreateLabel.amount += 1
 
+    def destroy_widget(self):
+        try:
+            self.grid_forget()
+            self.amount -= 1
 
-def destroy_object(widget):
-    try:
-        widget.grid_forget()
-    except:
-        print('there is no object')
-
-
-hello_maker = Label(
-    window,
-    text='hello'
-)
+        except:
+            print('there is no object')
 
 
 # Simple text on top of window
 enter_text = Label(
-    window,
-    text='Please enter something ToDo',
-    font=('JetBrains Mono', 10),
-)
+    window, text='Please enter something ToDo', font=('JetBrains Mono', 10))
 
 
 # TextBox Button
 text_box = Entry(
-    window,
-    background='light yellow',
-    width=35,
-    bd=2,
-)
+    window, background='light yellow', width=35, bd=2)
 
 
 # Submit Button
-begin = LabelUpdate()
-start = begin.common_command
 submit_btn = Button(
-    window,
-    text='Submit',
-    width=5,
-    bd=2,
-    activeforeground='green',
-    command=start
-)
+    window, text='Submit', width=5, bd=2, activeforeground='green', command=lambda: CreateLabel(text_box.get()))
 
 
 # Number of ToDo Lists
@@ -72,8 +55,7 @@ todo_length = Label(
     font=('JetBrains Mono', 10),
     foreground='red',
     background='black',
-    highlightbackground='black'
-)
+    highlightbackground='black')
 
 
 # Quit Button
@@ -82,8 +64,7 @@ quit_btn = Button(
     text='Quit',
     width=6,
     activeforeground='red',
-    command=window.destroy
-)
+    command=window.destroy)
 
 Button(
     window,
@@ -91,15 +72,16 @@ Button(
     width=5,
     bd=2,
     activeforeground='green',
-    command=lambda: destroy_object(begin.common)
+    command=lambda: CreateLabel.destroy_widget()
 ).grid(row=5, column=2)
 
 
-enter_text.grid(row=0, column=1, columnspan=2)
-text_box.grid(row=1, column=1, columnspan=2)
-submit_btn.grid(row=2, column=1, columnspan=2)
-todo_length.grid(row=6, column=2, padx=10)
-quit_btn.grid(row=6, column=1, columnspan=2)
+enter_text.grid(row=0, column=0, columnspan=2)
+text_box.grid(row=1, column=0, columnspan=2)
+submit_btn.grid(row=2, column=0, columnspan=2)
+todo_length.grid(row=6, column=0, sticky='SW')
+quit_btn.grid(row=6, column=1, columnspan=2, sticky='SE')
+
 window.grid_rowconfigure(5, weight=1)
 window.columnconfigure(1, weight=1)
 
