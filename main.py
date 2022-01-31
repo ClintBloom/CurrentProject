dd22bb8982ac2e397db5ad63df5b64b3db0194fd
Clint Bloomquist <clintbloomquist@gmail.com>
2022-01-31 14:11:33 -0700

    Updated todo left

--- main.py
+++ main.py
@@ -2,33 +2,17 @@ import tkinter
 from tkinter import Label
 from tkinter import Button
 from tkinter import Entry
-from time import sleep
 
 
 window_title = 'ToDo List'
-
 window = tkinter.Tk()
 window.title(window_title)
 window.geometry("400x200")
 window.resizable(False, False)
 
 
-def todo_amount(amount):
-    todo_length = Label(
-        window,
-        text=f'ToDo\'s left {amount}',
-        font=('JetBrains Mono', 10),
-        foreground='red',
-        background='black',
-        highlightbackground='black')
-
-    todo_length.grid_forget()
-    todo_length.grid(row=6, column=0, sticky='SW')
-
-
 class CreateLabel:
     amount = 0
-    todo_amount(amount)
     max_todos = []
 
     def __init__(self, todo):
@@ -38,16 +22,34 @@ class CreateLabel:
             CreateLabel.amount += 1
 
 
+def todo_amount():
+    todo_length = Label(
+        window,
+        text=f'ToDo\'s left {CreateLabel.amount}',
+        font=('JetBrains Mono', 10),
+        foreground='red',
+        background='black',
+        highlightbackground='black')
+    todo_length.grid_forget()
+    todo_length.grid(row=6, column=0, sticky='SW')
+
+
 def destroy_widget():
     if 5 >= len(CreateLabel.max_todos) > 0:
         CreateLabel.max_todos[CreateLabel.amount - 1].grid_forget()
         CreateLabel.max_todos.pop(CreateLabel.amount - 1)
         CreateLabel.amount -= 1
+        todo_amount()
 
 
-def testing():
+def submit_button():
     CreateLabel(text_box.get())
     text_box.delete(0, 'end')
+
+
+def combine():
+    submit_button()
+    todo_amount()
 
 
 # TextBox Button
@@ -60,8 +62,7 @@ enter_text = Label(
 
 # Submit Button
 submit_btn = Button(
-    window, text='Submit', width=5, bd=2, activeforeground='green', command=testing
-)
+    window, text='Submit', width=5, bd=2, activeforeground='green', command=combine)
 
 # Quit Button
 quit_btn = Button(
@@ -71,21 +72,22 @@ quit_btn = Button(
     activeforeground='red',
     command=window.destroy)
 
-Button(
+# Delete Button
+delete_btn = Button(
     window,
     text='Delete',
     width=5,
     bd=2,
     activeforeground='red',
-    command=destroy_widget
-).grid(row=5, column=2)
+    command=destroy_widget)
 
 
 enter_text.grid(row=0, column=0, columnspan=2)
 text_box.grid(row=1, column=0, columnspan=2)
 submit_btn.grid(row=2, column=0, columnspan=2)
-
+delete_btn.grid(row=5, column=2)
 quit_btn.grid(row=6, column=1, columnspan=2, sticky='SE')
+
 
 window.grid_rowconfigure(5, weight=1)
 window.columnconfigure(1, weight=1)
